549efa448eef4dd4ced0f750e2cb4f4a840cd1db
Osef1712 <j.zumkir@ludus-academie.com>
2021-03-31 15:29:39 +0200

    Ajout_exemple

--- Vecteur_Surcharge_TP_CPP/Vecteur_Surcharge_TP_CPP/main.cpp
+++ Vecteur_Surcharge_TP_CPP/Vecteur_Surcharge_TP_CPP/main.cpp
@@ -6,8 +6,16 @@ int main() {
 	CVect3D v2(2, 2, 2);
 	CVect3D v3;
 	CVect3D v4(1, 1, 1);
-	v3 = v1 - v2;
+	CVect3D v5;
+	CVect3D v6 = v2;
+	v3 = v1 + v2;
+	v5 = v1 - v2;
+	v5 = v2 * v3;
+	v5 = v5 * 2;
 	if (v1 == v4) {
+		v1.AfficheLog();
+	}
+	if (v1 == v3) {
 		v1.AfficheLog();
 	}
 	if (coincide3d(v1, v4)) {
