a8d09dce13cfe85b899e3aefa61bfcb11e355c3e
Osef1712 <j.zumkir@ludus-academie.com>
2021-03-31 15:33:53 +0200

    Ajout_exemple

--- Vecteur_Surcharge_TP_CPP/Vecteur_Surcharge_TP_CPP/main.cpp
+++ Vecteur_Surcharge_TP_CPP/Vecteur_Surcharge_TP_CPP/main.cpp
@@ -4,6 +4,11 @@ int main() {
 	CVect2D v1(1,1);
 	CVect2D v2 = v1;
 	CVect2D v3 = v1.SomVect(v2);
+	v3 = v1.SousVect(v2);
+
+	CVect2D v4(3, 3);
+	CVect2D v5;
+	v5 = v4.MultVect(v3);
 	
 	float z = v1.MultVect(v3);
 
