a28079afb6587a1db7e97856573baba18dab2157
Osef1712 <j.zumkir@ludus-academie.com>
2021-03-31 15:31:48 +0200

    Ajout_exemple

--- Vecteur_Surcharge_TP_CPP/Vecteur_Surcharge_TP_CPP/main.cpp
+++ Vecteur_Surcharge_TP_CPP/Vecteur_Surcharge_TP_CPP/main.cpp
@@ -5,8 +5,15 @@ int main() {
 	CVect2D v2 = v1;
 
 	CVect2D v3;
+	v3 = v1 + v2;
 	v3 = v1 - v2;
-	v1.AfficheLog();
+	CVect2D v4(3, 3);
+	CVect2D v5;
+	v5 = v3 * v4;
+
+	if (v1 == v2) {
+		v1.AfficheLog();
+	}
 	if (v1 != v3) {
 		v1.AfficheLog();
 	}
